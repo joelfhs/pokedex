cc527de898fa0191cace1ca12ca770ffa7e77276
Joel Huamanciza <joelfhs23@gmail.com>
2022-10-19 21:54:03 -0500

    vista para los pokemones, y primera comsumo del api con axios

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-for="pokemon in pokemonList" class="col-12 co-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <img src="#" class="card-img-top p-2" :alt="pokemon.name">
+      
+      <div class="card-body">
+        <h3 class="card-title">{{ pokemon.name }}</h3>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        //pokemonLink: ""
+      }
+    },
+    props: ["pokemonList"],
+  };
+</script>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,13 +1,42 @@
 <template>
-  <h1 class="text-center">Pokemones:</h1>
+  <div class="container bg-light">
+    <h1 class="text-center">{{ title }}:</h1>
+    <div class="row">
+
+        <pokemon-list :pokemonList="pokemonList"></pokemon-list>
+
+    </div>
+  </div>
 </template>
 
 
 <script>
+  import axios from 'axios';
+  import pokemonList from "../components/PokemonList.vue";
+  export default { 
+    components: {
+        pokemonList,
+    },
+    data() {
+      return {
+        title: "Pokemones",
+        pokemonList: [],
+      }
+    },
+    methods: {
+      async loadPokemons() {
+        await axios.get('https://pokeapi.co/api/v2/pokemon/')
+        .then((response) => {
+          this.pokemonList = response.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      },
+    },
 
+    async mounted() {
+      await this.loadPokemons();
+    },
+  }
 </script>
-
-
-<style>
-
-</style>
