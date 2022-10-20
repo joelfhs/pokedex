e0a923ee41a52427b38ba7d979ac0ea03479715d
Joel Huamanciza <joelfhs23@gmail.com>
2022-10-19 21:28:43 -0500

    Intalacion de Vue y VueRouter

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand" to="/">Pokedex</RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Pokemones</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/favoritos">Favoritos</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+
+<style scoped>
+</style>
---
+++ src/views/PokemonView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-center">Pokemones:</h1>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+
+</style>
